92297cd8c4404ae2441ef59d9f8575d8953e0e82
Iocqs <88013871+Iocqs@users.noreply.github.com>
2022-02-15 04:41:31 -0500

    hey

---
+++ games/3652625463.lua
@@ -0,0 +1,16 @@
+local Player = game:GetService("Players").LocalPlayer
+local Mouse = Player:GetMouse()
+local Clicking = false
+Mouse.KeyDown:Connect(
+    function(Key)
+        if Key == "v" then -- You can change "v" to whatever Key you want!
+            Clicking = not Clicking
+            if Clicking == true then
+                repeat
+                    mouse1click()
+                    wait(0.5) -- Don't go below 0.5. Unless you want 0 frames.
+                until Clicking == false
+            end
+        end
+    end
+)
