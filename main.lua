fb9a2fd8f160595b991e30ceea91c0fa9e051b7e
Iocqs <88013871+Iocqs@users.noreply.github.com>
2022-02-15 04:52:50 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,10 @@
-game.Player.LocalPlayer:Kick("soon")
+--simple loader cuz lazy
+local success, returned = pcall(function() 
+    return game:HttpGet("https://raw.githubusercontent.com/Iocqs/salad.tk/main/games/" .. game.PlaceId .. ".lua")
+end)
+if not (success and returned and #returned ~= 0) then 
+    return error("Game not supported!")
+end
+
+print("Game supported!")
+loadstring(tostring(returned))()
